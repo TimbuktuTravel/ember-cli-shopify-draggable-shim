a37153a9d8f4a887c1b6206d6aa701a2fb245d52
ianpetzer <ianpetzer@gmail.com>
2018-03-13 17:44:01 +0200

    Try use the legacy bundle

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ module.exports = {
 
   included() {
     this._super.included.apply(this, arguments);
-    this.import('vendor/draggable.bundle.js');
+    this.import('vendor/draggable.bundle.legacy.js');
     this.import('vendor/shims/shopify-draggable.js');
   },
 
   treeForVendor(vendorTree) {
-		var draggablePath = path.dirname(require.resolve('@shopify/draggable/lib/draggable.bundle.js'));
-		var draggableTree = new Funnel(draggablePath, {
-      files: ['draggable.bundle.js']
+    var draggablePath = path.dirname(require.resolve('@shopify/draggable/lib/draggable.bundle.legacy.js'));
+    var draggableTree = new Funnel(draggablePath, {
+      files: ['draggable.bundle.legacy.js']
     });
 
     return new MergeTrees([vendorTree, draggableTree]);
